4332ead31a5f23d408d6df73f518939c897fcf17
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-23 22:54:34 +0900

    Add files via upload

---
+++ htf-5/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  htf-emoji
+//
+//  Created by Dhanraj Chavan on 23/10/21.
+//
+
+import Foundation
+
+struct Constants {
+    struct MyDict {
+        static let nums = [
+            "0️⃣":"Zero", "1️⃣":"One", "2️⃣":"Two", "3️⃣":"Three", "4️⃣":"Four", "5️⃣":"Five", "6️⃣":"Six", "7️⃣":"Seven", "8️⃣":"Eight", "9️⃣":"Nine", "🔟":"Ten"
+        ]
+        static let commons = [
+            "👋":"Hi", "👍":"Yes", "🙅‍♂️":"No", "👌":"Ok", "🙏":"Please", "🙋‍♂️":"Me", "☺️":"You're welcome", "🙌":"Yup", "👏":"Congrats", "✌️":"Good Bye", "🤟":"Love You", "✨":"Nice", "❔":"How are you"
+        ]
+        static let greetings = [
+            "☀️":"Good Morning", "🌙":"Good Night", "⛅️":"Good Afternoon", "🌆":"Good Evening", "✨":"Have a nice day", "🤗":"Hope you are doing well"
+        ]
+        static let reactions = [
+            "🤗":"Thank you so much", "🤝":"Nice to meet to you", "🤯":"Mind Blowing", "😱":"Oh My God", "🥳":"Congratulations", "😆":"LOL", "🧐":"What is this?", "🤩":"Awesome", "🤒":"Not feeling well", "👻":"Surpise", "😵":"Oh no", "☹️":"Really sad", "😮":"Ohh", "🤷‍♂️":"I don't know"
+        ]
+        static let instructions = [
+            "🤫":"Keep Quiet", "🙄":"Look Up", "🏃‍♂️":"Run", "🤔":"Think", "⬆️":"Up", "⬇️":"Down", "⬅️":"Left", "➡️":"Right", "▶️":"Play", "⏸":"Pause", "🦻":"Listen", "👀":"Look", "💧":"Drop", "✍️":"Write"
+        ]
+        static let timings = [
+            "📅":"Today", "🌙":"Tonight", "⏪":"Yesterday", "⏩":"Tommorrow", "🔂":"Next Week", "🗓":"Next Month"
+        ]
+    }
+}
